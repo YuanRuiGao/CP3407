cffa268215988f8122b63f39bb78af5e697e680a
YuanRuiGao <yuanrui.gao@my.jcu.edu.au>
2025-07-23 09:49:41 +0800

    test

--- House Cleaning App version1/House Cleaning App version1/mobileass2-main/app/src/main/java/com/example/myapplication_ass2/AppSettingsViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication_ass2
-
-import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.ViewModel
-
-class AppSettingsViewModel : ViewModel() {
-    var fontSize = mutableStateOf(16f)
-    var isDarkMode = mutableStateOf(false)
-}
--- House Cleaning App version1/House Cleaning App version1/mobileass2-main/app/src/test/java/com/example/myapplication_ass2/FakeDrugRepository.kt
+++
@@ -1,5 +0,0 @@
-package com.example.myapplication_ass2.repository
-
-import com.example.myapplication_ass2.network.DrugApiService
-
-class FakeDrugRepository(apiService: DrugApiService) : DrugRepository(apiService)
